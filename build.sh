dec322bbfb7fa3d24c8a4377af7ccdab6133d4b1
Nick Walker <nickwalk@microsoft.com>
2017-06-16 10:26:42 -0700

    Adding latest tag to build script

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -x -e
 
-docker build -t "$1"/kudu:1.7 1.7
+docker build -t "$1"/kudu:1.7 -t "$1"/kudu:latest 1.7
 
 docker login -u "$2" -p "$3"
 
 docker push "$1"/kudu:1.7
+docker push "$1"/kudu:latest
 
 docker logout
