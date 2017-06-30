60bea183df9a8dd1092a7a3dd036374eea33ec38
Nick Walker <nickwalk@microsoft.com>
2017-06-30 10:35:28 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -x -e
 
-docker build -t "$1"/kudu:1.7 -t "$1"/kudu:latest 1.7
+buildnumber=${4-$(date -u +"%y%m%d%H%M")}
+
+docker build -t "$1"/kudu:1.7_"$buildnumber" -t "$1"/kudu:latest_"$buildnumber" 1.7
 
 docker login -u "$2" -p "$3"
 
-docker push "$1"/kudu:1.7
-docker push "$1"/kudu:latest
+docker push "$1"/kudu:1.7_"$buildnumber"
+docker push "$1"/kudu:latest_"$buildnumber"
 
 docker logout
