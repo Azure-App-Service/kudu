de8d3783a9c9fe50f6b9c1b4261a6dd72d877b7d
Michimune Kohno <michimune@outlook.com>
2017-11-02 17:14:38 -0700

    Add log analyzer

--- 1.7/startup.sh
+++ 1.7/startup.sh
@@ -49,7 +49,7 @@ export SCM_BIN_PATH=/opt/Kudu/bin
 # mod-mono-server startup with apache startup, resulting in faster cold start.
 # mod_mono will still spawn new instances in the event that this one exits early.
 runuser -p -u "$USER_NAME" -- /usr/bin/mono /usr/lib/mono/4.5/mod-mono-server4.exe \
-  --filename /tmp/mod_mono_server_default --applications /:/opt/Kudu --nonstop &
+  --filename /tmp/mod_mono_server_default --applications /:/opt/Kudu,/loganalyzer:/opt/LogAnalyzer --nonstop &
 
 while [ ! -S /tmp/mod_mono_server_default ] ; do
  sleep 1
