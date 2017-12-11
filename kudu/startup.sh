c3e8fdcf90f6d08ad86b74852211355386ab3eaa
Nick Walker <nickwalk@microsoft.com>
2017-12-11 12:23:41 -0800

    Up and running, now iterate

--- kudu/startup.sh
+++ kudu/startup.sh
@@ -12,23 +12,8 @@ SITE_NAME=$5
 
 groupadd -g $GROUP_ID $GROUP_NAME
 useradd -u $USER_ID -g $GROUP_NAME $USER_NAME
-chown -R $USER_NAME:$GROUP_NAME /etc/apache2
 chown -R $USER_NAME:$GROUP_NAME /opt/Kudu
-touch /var/log/apache2/kudu-error.log
-touch /var/log/apache2/kudu-access.log
-mkdir -p /var/lock/apache2 /var/run/apache2
-chown -R $USER_NAME:$GROUP_NAME /var/log/apache2 /var/lock/apache2 /var/run/apache2
 chown -R $USER_NAME:$GROUP_NAME /tmp
-sed -i -- "s/KuduSite/$SITE_NAME/g" /etc/apache2/sites-available/kudu.conf
-sed -i -- "s/user-placeholder/$USER_NAME/g" /etc/apache2/apache2.conf
-sed -i -- "s/group-placeholder/$GROUP_NAME/g" /etc/apache2/apache2.conf
-sed -i -- "s/80/8080/g" /etc/apache2/ports.conf
-mkdir -p /etc/mono/registry
-chmod uog+rw /etc/mono/registry
-chown -R $USER_NAME:$GROUP_NAME /etc/mono/registry
-cd /etc/apache2/sites-available
-a2dissite 000-default.conf
-a2ensite kudu.conf
 
 /bin/bash -c "node /opt/webssh/index.js &"
 
@@ -42,18 +27,5 @@ export KUDU_MSBUILD=/usr/bin/xbuild
 export APPDATA=/opt/Kudu/local
 export SCM_BIN_PATH=/opt/Kudu/bin
 
-# Start mod-mono-server and give it a chance to warm up before
-# hitting it with requests. This prevents a pathological behavior
-# in mod_mono where it keeps starting new mod-mono-server processes
-# because they aren't responding fast enough, and it parallelizes
-# mod-mono-server startup with apache startup, resulting in faster cold start.
-# mod_mono will still spawn new instances in the event that this one exits early.
-runuser -p -u "$USER_NAME" -- /usr/bin/mono /usr/lib/mono/4.5/mod-mono-server4.exe \
-  --filename /tmp/mod_mono_server_default --applications /:/opt/Kudu,/loganalyzer:/opt/LogAnalyzer --nonstop &
-
-while [ ! -S /tmp/mod_mono_server_default ] ; do
- sleep 1
-done
-
-#Run apache
-/usr/sbin/apache2ctl -D FOREGROUND
+cd /opt/Kudu
+ASPNETCORE_URLS=http://+:8181 runuser -p -u "$USER_NAME" -- dotnet Kudu.Services.Web.dll
