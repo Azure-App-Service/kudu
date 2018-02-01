9acfd5efd84dcf0fb2b5f528012be2ded55257e4
Lukasz Stempniewicz <lukaszs@microsoft.com>
2018-02-01 08:30:23 -0800

    Update build.sh to push :latest

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ set -x -e
 buildnumber=${4-$(date -u +"%y%m%d%H%M")}
 
 docker build -t "$1"/kudu:"$buildnumber" kudu
+docker tag "$1"/kudu:"$buildnumber" "$1"/kudu:latest
 
 docker login -u "$2" -p "$3"
 
 docker push "$1"/kudu:"$buildnumber"
+docker push "$1"/kudu:latest
 
 docker logout
