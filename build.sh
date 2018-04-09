1a8f771d22d5ab8fb910b02f651d7f061f3f4a58
Lukasz Stempniewicz <lukaszcorgi@gmail.com>
2018-04-09 14:55:21 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -x -e
 
 buildnumber=${4-$(date -u +"%y%m%d%H%M")}
 
-docker build -t "$1"/kudu:"$buildnumber" kudu
+docker build --no-cache -t "$1"/kudu:"$buildnumber" kudu
 docker tag "$1"/kudu:"$buildnumber" "$1"/kudu:latest
 
 docker login -u "$2" -p "$3"
